2f69ef105009c764ae327cb571d27bd1448e3bdd
Moiz Ali Siddiqui <siddiqui.moizali1@gmail.com>
2025-07-18 11:56:47 +0500

    Deposit and Number Of Lines Funtion

--- practice.js
+++ practice.js
@@ -1,3 +1,29 @@
+const prompt = require('prompt-sync')();
+
+
 const deposit = () => {
 
-}
+    while (true) {
+    const amount = prompt (`Enter the amount you want to deposit: `);
+    const numberamountdepost = parseFloat(amount);
+    if(isNaN(numberamountdepost) || numberamountdepost <= 0) {
+        console.log(`Invalid amount. Please enter a positive number.`);
+    }else{
+        return numberamountdepost;
+    }
+}};
+
+const NOL = ()=>{
+
+    while (true) {
+const lines = prompt(`Enter the number of lines you want to bet on (1-3):`);
+const numberOfLines = parseFloat(lines);
+
+if(isNaN(numberOfLines) || numberOfLines <1 || numberOfLines > 3) {
+    console.log(`Invalid number of lines. Please enter a number between 1 and 3.`);
+}else{
+    return numberOfLines;}}
+};
+
+let balance = deposit();
+const numberOfLines = NOL();
